2f00aee285ab54117299676e1a01a6837b49e4e5
aspasionov <spasia3run@gmail.com>
2023-04-16 21:50:36 +0300

    feat: add api methods

--- src/api/cards.api.ts
+++ src/api/cards.api.ts
@@ -1,9 +1,29 @@
 import instance from '@api/base.api'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const fetchAll = async () => {
+export const fetchAll = async (): Promise<unknown> => {
   return await instance({
     url: '/cards',
     method: 'GET'
+  })
+}
+
+export const deleteOne = async (id: string): Promise<unknown> => {
+  return await instance({
+    url: `/cards/${id}`,
+    method: 'DELETE'
+  })
+}
+
+export const addOne = async (): Promise<unknown> => {
+  return await instance({
+    url: '/cards',
+    method: 'POST'
+  })
+}
+
+export const updateOne = async (id: string): Promise<unknown> => {
+  return await instance({
+    url: `/cards/${id}`,
+    method: 'PUT'
   })
 }
--- src/api/columns.api.ts
+++ src/api/columns.api.ts
@@ -1,8 +1,29 @@
 import instance from '@api/base.api'
 
-export const fetchAll = () => {
-  return instance({
+export const fetchAll = async (): Promise<unknown> => {
+  return await instance({
     url: '/columns',
     method: 'GET'
+  })
+}
+
+export const deleteOne = async (id: string): Promise<unknown> => {
+  return await instance({
+    url: `/columns/${id}`,
+    method: 'DELETE'
+  })
+}
+
+export const addOne = async (): Promise<unknown> => {
+  return await instance({
+    url: '/columns',
+    method: 'POST'
+  })
+}
+
+export const updateOne = async (id: string): Promise<unknown> => {
+  return await instance({
+    url: `/columns/${id}`,
+    method: 'PUT'
   })
 }
