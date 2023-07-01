7ad82ea22b236ea29919c22a8075680445b649c1
aspasionov <spasia3run@gmail.com>
2023-07-01 16:42:09 +0300

    feat: improve edit mode

--- src/screens/Home/Card.tsx
+++ src/screens/Home/Card.tsx
@@ -1,6 +1,21 @@
 import React, { useState } from 'react'
-import { CloseIcon } from '@chakra-ui/icons'
-import { Box, Flex, Input, Text, Heading, IconButton } from '@chakra-ui/react'
+import { ChevronDownIcon, HamburgerIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Flex,
+  Input,
+  Text,
+  Heading,
+  Stack,
+  FormControl,
+  FormLabel,
+  MenuButton,
+  Button,
+  MenuList,
+  MenuItem,
+  Menu,
+  Textarea
+} from '@chakra-ui/react'
 import { updateColumn, fetchColumns } from '@store/columns/asyncActions'
 import { useAppDispatch } from '@store/store'
 import type { CardProps } from 'react-trello-ts/dist/components/Card'
@@ -8,22 +23,11 @@ import type { CardT } from '@store/cards/types'
 import { useSelector } from 'react-redux'
 import { selectColumns } from '@store/columns/selectors'
 import type { ColumnT } from '@store/columns/types'
-
-interface EditableI {
-  title: boolean
-  label: boolean
-  description: boolean
-}
-
-const defaultState = {
-  title: false,
-  label: false,
-  description: false
-}
+import ModalWindow from '@components/ModalWindow'
 
 const Card: React.FC<CardProps & { columnId: string }> = (props) => {
   const { id, title, label, description } = props
-  const [editable, setEditable] = useState<EditableI>(defaultState)
+  const [modalOpen, setModalOpen] = useState(false)
   const [card, setCard] = useState<Partial<CardT>>({
     id,
     title: title ?? '',
@@ -35,32 +39,35 @@ const Card: React.FC<CardProps & { columnId: string }> = (props) => {
   const dispatch = useAppDispatch()
   const columns = useSelector(selectColumns)
 
-  const toggleEdit = async (
-    key: keyof EditableI,
-    isBlurEvent: boolean = false
-  ): Promise<any> => {
-    setEditable((prev) => ({ ...prev, [key]: !prev[key] }))
-    if (isBlurEvent) {
-      const currentColumn = columns.find((el) => el.id === card.columnId)
-      const updatedCurrentColumn: Partial<ColumnT> = {
-        ...currentColumn,
-        cards:
-          currentColumn === undefined
-            ? []
-            : [
-                ...currentColumn.cards.filter((item) => item.id !== card.id),
-                card as CardT
-              ]
-      }
-      await dispatch(updateColumn(updatedCurrentColumn as ColumnT))
+  const handleEdit = (): void => {
+    setModalOpen(true)
+  }
+
+  const onSubmit = async (): Promise<void> => {
+    const currentColumn = columns.find((el) => el.dbId === card.columnId)
+    const updatedCurrentColumn: Partial<ColumnT> = {
+      ...currentColumn,
+      cards:
+        currentColumn === undefined
+          ? []
+          : [
+              ...currentColumn.cards.filter((item) => item.id !== card.id),
+              card as CardT
+            ]
     }
+    await dispatch(updateColumn(updatedCurrentColumn as ColumnT))
+    setModalOpen(false)
   }
 
   const handleChange = ({
     target: { name, value }
-  }: React.ChangeEvent<HTMLInputElement>): void => {
+  }: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
     setCard((prev) => {
-      return { ...prev, [name]: value, columnId: props.columnId }
+      return {
+        ...prev,
+        [name]: value,
+        columnId: props.columnId
+      }
     })
   }
 
@@ -80,137 +87,106 @@ const Card: React.FC<CardProps & { columnId: string }> = (props) => {
   return (
     <Box p={2} mb={1} bg="white" maxWidth={250} rounded="md">
       <Flex align="center" justify="space-between">
-        {editable.title ? (
-          <Input
-            value={card.title}
-            name="title"
-            onChange={handleChange}
-            flexGrow={0}
-            flexShrink={0}
-            autoFocus
-            onBlur={() => {
-              void (async () => {
-                await toggleEdit('title', true)
-              })()
-            }}
-            w="43%"
-            placeholder="Title"
-            size="xs"
-          />
-        ) : (
-          <Heading
-            as="h6"
-            size="xs"
-            flexGrow={0}
-            textOverflow="ellipsis"
-            overflow="hidden"
-            w="43%"
-            flexShrink={0}
-            onClick={() => {
-              void (async () => {
-                await toggleEdit('title')
-              })()
-            }}
-          >
-            {card.title === '' ? (
-              <Text color="#ccc">Edit title</Text>
-            ) : (
-              card.title
-            )}
-          </Heading>
-        )}
-        {editable.label ? (
-          <Input
-            value={card.label}
-            name="label"
-            onChange={handleChange}
-            flexGrow={0}
-            flexShrink={0}
-            autoFocus
-            onBlur={() => {
-              void (async () => {
-                await toggleEdit('label', true)
-              })()
-            }}
-            w="43%"
-            placeholder="Label"
-            size="xs"
-          />
-        ) : (
-          <Heading
-            as="h6"
-            size="xs"
-            flexGrow={0}
-            textOverflow="ellipsis"
-            overflow="hidden"
-            w="43%"
-            flexShrink={0}
-            onClick={() => {
-              void (async () => {
-                await toggleEdit('label')
-              })()
-            }}
-          >
-            {card.label === '' ? (
-              <Text color="#ccc">Edit label</Text>
-            ) : (
-              card.label
-            )}
-          </Heading>
-        )}
+        <Heading
+          as="h6"
+          size="xs"
+          flexGrow={0}
+          textOverflow="ellipsis"
+          overflow="hidden"
+          w="43%"
+          flexShrink={0}
+        >
+          {card.title === '' ? (
+            <Text color="#ccc">No title yet</Text>
+          ) : (
+            card.title
+          )}
+        </Heading>
+
+        <Heading
+          as="h6"
+          size="xs"
+          flexGrow={0}
+          textOverflow="ellipsis"
+          overflow="hidden"
+          w="43%"
+          flexShrink={0}
+        >
+          {card.label === '' ? (
+            <Text color="#ccc">No label yet</Text>
+          ) : (
+            card.label
+          )}
+        </Heading>
       </Flex>
 
       <Flex mt={1} align="center" justify="space-between">
-        {editable.description ? (
-          <Input
-            value={card.description}
-            name="description"
-            onChange={handleChange}
-            flexGrow={1}
-            autoFocus
-            onBlur={() => {
-              void (async () => {
-                await toggleEdit('description', true)
-              })()
-            }}
-            placeholder="Description"
-            size="xs"
-          />
-        ) : (
-          <Heading
-            as="h6"
-            size="sm"
-            flexGrow={1}
-            textOverflow="ellipsis"
-            overflow="hidden"
-            w="90%"
-            flexShrink={0}
-            onClick={() => {
-              void (async () => {
-                await toggleEdit('description')
-              })()
-            }}
-          >
-            {card.description === '' ? (
-              <Text color="#ccc">Edit description</Text>
-            ) : (
-              card.description
-            )}
-          </Heading>
-        )}
-        <Box w="10%" sx={{ flex: '0 0 auto' }} flexGrow={0}>
-          <IconButton
-            aria-label="Delete task"
-            onClick={() => {
-              void (async () => {
-                await handleDelete()
-              })()
-            }}
-            icon={<CloseIcon />}
-            size="xs"
-          />
+        <Heading
+          as="h6"
+          size="sm"
+          flexGrow={1}
+          textOverflow="ellipsis"
+          overflow="hidden"
+          w="70%"
+          flexShrink={0}
+        >
+          {card.description === '' ? (
+            <Text color="#ccc">No description yet</Text>
+          ) : (
+            card.description
+          )}
+        </Heading>
+        <Box w="30%" sx={{ flex: '0 0 auto' }} flexGrow={0}>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <HamburgerIcon />
+            </MenuButton>
+            <MenuList>
+              <MenuItem onClick={handleEdit}>Edit</MenuItem>
+              <MenuItem
+                onClick={() => {
+                  void (async () => {
+                    await handleDelete()
+                  })()
+                }}
+              >
+                Delete
+              </MenuItem>
+            </MenuList>
+          </Menu>
         </Box>
       </Flex>
+
+      <ModalWindow
+        isOpen={modalOpen}
+        onClose={() => {
+          setModalOpen(false)
+        }}
+        onApply={() => {
+          void (async () => {
+            await onSubmit()
+          })()
+        }}
+      >
+        <Stack spacing={3} sx={{ pt: 10 }}>
+          <FormControl variant="floating" id="first-name">
+            <FormLabel>Title</FormLabel>
+            <Input value={card.title} name="title" onChange={handleChange} />
+          </FormControl>
+          <FormControl variant="floating" id="first-name">
+            <FormLabel>Label</FormLabel>
+            <Input value={card.label} name="label" onChange={handleChange} />
+          </FormControl>
+          <FormControl variant="floating" id="first-name">
+            <FormLabel>Description</FormLabel>
+            <Textarea
+              value={card.description}
+              name="description"
+              onChange={handleChange}
+            />
+          </FormControl>
+        </Stack>
+      </ModalWindow>
     </Box>
   )
 }
